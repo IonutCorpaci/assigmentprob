ef165400ec08a569a8413c9084854e4216222cb8
IonutCorpaci <corpaco2005@gmail.com>
2022-07-15 20:20:04 +0200

    initital commit

--- script.js
+++ script.js
@@ -12,25 +12,35 @@ class Student {
             "Phone: " + this.phone + "\n" +
             "Courses: " + this.courses + "\n\n";
     }
-    async getData() {
-        try {
-            let response = await fetch('https://v-dresevic.github.io/Advanced-JavaScript-Programming/data/students.txt')
-            if (response.status !== 200) {
-                throw new Error("Error while reading file..")
-            }
-            let text = await response.text();
-            var res = text.split("\n")
-        } catch (err) {
-            console.log('Fetch problem: ' + err.message)
+}
+
+async function getData() {
+    console.log("async call");
+    try {
+        let response = await fetch('https://v-dresevic.github.io/Advanced-JavaScript-Programming/data/students.txt')
+        if (response.status !== 200) {
+            throw new Error("Error while reading file..")
         }
+        let text = await response.text();
+        var res = text.split("\n")
+        console.log("Size of strings array: " + res.length);
         for(let i=0; i<=(res.lenght-3);i+=3) {
-            let student = new Student(res[i], res[i+1], res[i+2], res[i+3]);
+            var student = new Student(res[i], res[i+1], res[i+2], res[i+3]);
+            console.log(student.getInfo());
         }
+    } catch (err) {
+        console.log('Fetch problem: ' + err.message)
+    } finally {
+        let textArea = document.getElementById('text_area');
+        textArea.innerHTML = student.getInfo();
     }
 }
-console.log(student.getInfo());
-//console.log(Student.getInfo());
-//console.log(getInfo());
+
+
+
+
+
+
 
 
 
